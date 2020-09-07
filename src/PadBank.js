dd292eb2c47ed0954b04c0ab2e3dfc4044b17f9f
Krati Chauhan <kratis9@gmail.com>
2020-09-07 11:19:17 +1000

    initial commit react-drum-pad

---
+++ src/PadBank.js
@@ -0,0 +1,35 @@
+import React from "react";
+import DrumPad from "./DrumPad";
+class PadBank extends React.Component {
+  render() {
+    let padBank;
+    this.props.power
+      ? (padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+          return (
+            <DrumPad
+              clipId={padBankArr[i].id}
+              clip={padBankArr[i].url}
+              keyTrigger={padBankArr[i].keyTrigger}
+              keyCode={padBankArr[i].keyCode}
+              updateDisplay={this.props.updateDisplay}
+              power={this.props.power}
+            />
+          );
+        }))
+      : (padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+          return (
+            <DrumPad
+              clipId={padBankArr[i].id}
+              clip='#'
+              keyTrigger={padBankArr[i].keyTrigger}
+              keyCode={padBankArr[i].keyCode}
+              updateDisplay={this.props.updateDisplay}
+              power={this.props.power}
+            />
+          );
+        }));
+    return <div className='pad-bank'>{padBank}</div>;
+  }
+}
+
+export default PadBank;
